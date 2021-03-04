fe0541b0c7582175d3dcda1931027b8c79dd8c0d
Matt Larsen <matt.larsen@connorgp.com>
2021-03-04 16:58:58 -0700

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/matt-larsen-sld/sld_secrets_getter',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
