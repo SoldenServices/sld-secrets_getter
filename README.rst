0cce3a094713fab51546c4d7522cf2520aeecb4d
Matt Larsen <matt.larsen@connorgp.com>
2021-03-04 14:30:07 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,21 @@
+==================
+SLD Secrets Getter
+==================
+
+Wrapper for the AWS Secrets Manager libraries for getting secrets in a standard way.
+
+* Free software: MIT license
+* Documentation: https://sld-secrets-getter.readthedocs.io.
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Matthew Larsen",
+    author_email='matt.larsen@connorgp.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Wrapper for the AWS Secrets Manager libraries for getting secrets in a standard way.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='secrets_getter',
+    name='sld-secrets_getter',
+    packages=find_packages(include=['secrets_getter', 'secrets_getter.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/matt-larsen-sld/sld_secrets_getter',
+    version='0.1.0',
+    zip_safe=False,
+)
