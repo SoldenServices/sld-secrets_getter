f467c2459a5811a20fe6f3431b0447e27270fc77
Matt Larsen <matt.larsen@connorgp.com>
2021-03-04 17:03:34 -0700

    Change desc content type

--- secrets_getter/__init__.py
+++ secrets_getter/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Matthew Larsen"""
 __email__ = 'matt.larsen@connorgp.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .secrets_getter import SecretsManagerService
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst::q!",
     include_package_data=True,
     keywords='Secrets Manager',
     name='sld-secrets_getter',
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/matt-larsen-sld/sld_secrets_getter',
-    version='0.2.0',
+    version='0.2.1',
     zip_safe=False,
 )
